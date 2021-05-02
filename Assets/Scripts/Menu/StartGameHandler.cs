4e2b1c6a5b5cb01475a48ca0fc2301aa27312e15
stevenwhatever123 <60275831+stevenwhatever123@users.noreply.github.com>
2021-05-02 16:36:36 +0100

    Updated Start Game Scene

--- Assets/Scripts/Menu/StartGameHandler.cs
+++ Assets/Scripts/Menu/StartGameHandler.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class StartGameHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public TMP_Text text;
+    
     void Update()
     {
-        
+        Color();
+    }
+    
+    public void StartGame()
+    {
+        Debug.Log("Start Game");
+    }
+
+    public void Color()
+    {
+        if (EventSystem.current.currentSelectedGameObject == this.gameObject)
+        {   
+            text.color = UnityEngine.Color.yellow;
+        }
+        else
+        {
+            text.color = UnityEngine.Color.white;
+        }
     }
 }
