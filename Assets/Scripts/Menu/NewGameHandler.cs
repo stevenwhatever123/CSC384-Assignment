e31f5ebe9a945c78cc6718ed6be2b8f395705710
stevenwhatever123 <60275831+stevenwhatever123@users.noreply.github.com>
2021-05-02 19:25:55 +0100

    Added transition for MenuScene

--- Assets/Scripts/Menu/NewGameHandler.cs
+++ Assets/Scripts/Menu/NewGameHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -10,6 +11,15 @@ using UnityEngine.UI;
 public class NewGameHandler : MonoBehaviour
 {
     public Text text;
+
+    public Animator animator;
+
+    public float transitionTime = 1f;
+
+    void Start()
+    {
+        animator = GameObject.Find("Transition").GetComponent<Animator>();
+    }
     
     void Update()
     {
@@ -18,7 +28,15 @@ public class NewGameHandler : MonoBehaviour
     
     public void CreateNewGame()
     {
+        StartCoroutine(Transition());
         Debug.Log("New Game");
+    }
+
+    IEnumerator Transition()
+    {
+        animator.SetTrigger("Play");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene("NewGameMenu", LoadSceneMode.Single);
     }
 
     public void Color()
