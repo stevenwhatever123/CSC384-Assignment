6e2f25c7198f22e6eb8ff1384a1da67404a82b4c
stevenwhatever123 <60275831+stevenwhatever123@users.noreply.github.com>
2021-05-02 19:43:59 +0100

    Added Back button and transition for NewGameMenu

--- Assets/Scripts/Menu/NewGameBackButtonHandler.cs
+++ Assets/Scripts/Menu/NewGameBackButtonHandler.cs
@@ -1,51 +1,29 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class StartGameHandler : MonoBehaviour
+public class NewGameBackButtonHandler : MonoBehaviour
 {
-    public PlayerData playerData;
-
-    public TMP_InputField inputField;
-    
     public TMP_Text text;
-
-    public TMP_Text alertText;
-
+    
     private Animator animator;
 
     public float transitionTime = 1f;
-
+    
+    // Start is called before the first frame update
     void Start()
     {
         animator = GameObject.Find("Transition").GetComponent<Animator>();
     }
-    
+
     void Update()
     {
         Color();
     }
     
-    public void StartGame()
-    {
-        if (inputField.text != "")
-        {
-            alertText.color = new Color(1, 0, 0, 0);
-            alertText.enabled = false;
-            playerData.SetName(inputField.text);
-            StartCoroutine(Transition());
-            Debug.Log("Start Game");
-        }
-
-        alertText.color = new Color(1, 0, 0, 1);
-    }
-
     public void Color()
     {
         if (EventSystem.current.currentSelectedGameObject == this.gameObject)
@@ -57,11 +35,16 @@ public class StartGameHandler : MonoBehaviour
             text.color = UnityEngine.Color.white;
         }
     }
+
+    public void BackToMenu()
+    {
+        StartCoroutine(Transition());
+    }
     
     IEnumerator Transition()
     {
         animator.SetTrigger("Play");
         yield return new WaitForSeconds(transitionTime);
-        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+        SceneManager.LoadScene("MenuScene", LoadSceneMode.Single);
     }
 }
